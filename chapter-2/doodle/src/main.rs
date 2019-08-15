544583c3302672936f553b39f78636dc006e105e
tarberd <bernardo.mferrari@gmail.com>
2019-08-15 15:55:51 -0300

    infix digit tokenizer

--- chapter-2/doodle/src/main.rs
+++ chapter-2/doodle/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
+
+    let input = "5 + 9";
+
+    let mut tokens = vec![];
+
+    for character in input.chars() {
+        if character.is_ascii_digit() {
+            tokens.push("digit");
+        }
+
+        if character == '+' {
+            tokens.push("+");
+        }
+    }
+
+    println!("{:?}", tokens);
 }
